37bb10b1f3d2b8bb8e65c02d91d48cae3288f93d
zaelynnperez <72786696+zaelynnperez@users.noreply.github.com>
2020-12-11 20:25:45 -0500

    Update pxt.json, main.ts, main.py

--- main.ts
+++ main.ts
@@ -1,11 +1,20 @@
+// while True:
+//     print("light level: " + input.light_level())
+//     if input.light_level() > 5:
+//         light.set_all(light.rgb(0, 0, 0))
+//    elif input.light_level() < 5:
+//         light.set_all(light.rgb(255, 255, 255))
+//     else :
+//         print ("Circuit has ran into error")
 while (true) {
-    console.log("light level: " + input.lightLevel())
-    if (input.lightLevel() > 5) {
-        light.setAll(light.rgb(0, 0, 0))
-    } else if (input.lightLevel() < 5) {
-        light.setAll(light.rgb(255, 255, 255))
+    console.log("Light level: " + input.lightLevel())
+    if (input.lightLevel() < 4) {
+        light.setAll(light.rgb(0, 0, 255))
+    } else if (input.lightLevel() >= 12) {
+        light.clear()
     } else {
-        console.log("Circuit has ran into error")
+        input.lightLevel() >= 4 && 4 < 12
+        light.setAll(light.rgb(255, 255, 0))
     }
     
 }
